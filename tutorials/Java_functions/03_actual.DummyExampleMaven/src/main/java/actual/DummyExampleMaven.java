01d8de0176dac2eac986b758bd0c83496de8be60
Istemi Ekin Akkus <iea__dev@mpi-sws.org>
2020-07-17 10:38:28 +0200

    tutorial on using java functions

---
+++ tutorials/Java_functions/03_actual.DummyExampleMaven/src/main/java/actual/DummyExampleMaven.java
@@ -0,0 +1,41 @@
+/*
+   Copyright 2020 The KNIX Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+package actual;
+
+import org.microfunctions.mfnapi.MicroFunctionsAPI;
+
+import org.json.JSONObject;
+
+import dummy.SomeClass;
+
+public class DummyExampleMaven
+{
+    public Object handle(Object event, MicroFunctionsAPI context)
+    {
+        SomeClass sc = new SomeClass("myname", 10);
+        sc.doSomething(context);
+        
+        String eventstr = (String) event;
+
+        JSONObject obj = new JSONObject();
+        obj.put("input", eventstr);
+        obj.put("output", "Finished: " + eventstr);
+        
+        return obj;
+    }
+}
+
