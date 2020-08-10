3f9218cc15542a889010020868713f23b8b0469e
ksatzke <klaus.satzke@nokia-bell-labs.com>
2020-08-10 14:55:10 +0200

    removing unused classes, cleaning up

--- FunctionWorker/python/MicroFunctionsAPI.py
+++ FunctionWorker/python/MicroFunctionsAPI.py
@@ -24,6 +24,27 @@ import py3utils
 import requests
 import json
 
+from typing import Dict, Any
+from datetime import date
+
+LambdaDict = Dict[str, Any]
+
+class LambdaCognitoIdentity(object):
+    cognito_identity_id: str
+    cognito_identity_pool_id: str
+
+class LambdaClientContextMobileClient(object):
+    installation_id: str
+    app_title: str
+    app_version_name: str
+    app_version_code: str
+    app_package_name: str
+
+class LambdaClientContext(object):
+    client: LambdaClientContextMobileClient
+    custom: LambdaDict
+    env: LambdaDict
+
 class MicroFunctionsAPI:
     '''
     This class defines the API that is exposed to the user functions.
@@ -54,37 +75,7 @@ class MicroFunctionsAPI:
             None
         '''
 
-        from typing import Dict, Any
-        from datetime import date
-
-        LambdaDict = Dict[str, Any]
-
-        class LambdaCognitoIdentity(object):
-            cognito_identity_id: str
-            cognito_identity_pool_id: str
-
-        class LambdaClientContextMobileClient(object):
-            installation_id: str
-            app_title: str
-            app_version_name: str
-            app_version_code: str
-            app_package_name: str
-
-        class LambdaClientContext(object):
-            client: LambdaClientContextMobileClient
-            custom: LambdaDict
-            env: LambdaDict
-
-        class LambdaContext(object):
-            function_name: str
-            function_version: str
-            invoked_function_arn: str
-            memory_limit_in_mb: int
-            aws_request_id: str
-            log_group_name: str
-            log_stream_name: str
-            identity: LambdaCognitoIdentity
-            client_context: LambdaClientContext
+        
 
  
         self._logger = logger
